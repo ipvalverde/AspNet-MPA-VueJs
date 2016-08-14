295a090accd96cf7101d017bb5c50d48c5bc3048
Israel Valverde <israelpereiravalverde@gmail.com>
2016-08-14 14:08:23 -0300

    Using external reference to Vue

--- src/AspNetMultiPageApplicationWithVueJs/AspNetMultiPageApplicationWithVueJs/gulpfile.js
+++ src/AspNetMultiPageApplicationWithVueJs/AspNetMultiPageApplicationWithVueJs/gulpfile.js
@@ -1,34 +1,45 @@
-﻿/// <binding BeforeBuild='webpack' />
+﻿/// <binding BeforeBuild='webpack:build-dev' />
 "use strict";
 
 var gulp = require("gulp"),
     webpack = require("webpack"),
     gutil = require("gulp-util");
 
+gulp.task('copy-dependencies', function () {
+    return gulp.src('./node_modules/vue/dist/*.js')
+        .pipe(gulp.dest('./Scripts/vue/'));
+})
+
 gulp.task("webpack", function (callback) {
 
     // run webpack
-    webpack({
-        entry: {
-            contactEdit: './scripts/src/contact/edit.js',
-            contactSearch: './scripts/src/contact/search.js'
-        },
-        output: {
-            path: './scripts/built',
-            filename: '[name].bundle.js'
-        },
-        module: {
-            loaders: [{
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-            }]
-        }
-    }, function (err, stats) {
+    webpack(require('./webpack.config.js'), function (err, stats) {
         if (err) throw new gutil.PluginError("webpack", err);
         gutil.log("[webpack]", stats.toString({
             // output options
         }));
         callback();
     });
+});
+
+var myDevConfig = require('./webpack.config.js');
+myDevConfig.devtool = "sourcemap";
+myDevConfig.debug = true;
+
+// create a single instance of the compiler to allow caching
+var devCompiler = webpack(myDevConfig);
+
+gulp.task('webpack:build-dev', function (callback) {
+    // run webpack
+    devCompiler.run(function (err, stats) {
+        if (err) throw new gutil.PluginError("webpack:build-dev", err);
+        gutil.log("[webpack:build-dev]", stats.toString({
+            colors: true
+        }));
+        callback();
+    });
+});
+
+gulp.task('watch-webpack', function () {
+    gulp.watch(["scripts/src/**/*"], ["webpack:build-dev"]);
 });
---
+++ src/AspNetMultiPageApplicationWithVueJs/AspNetMultiPageApplicationWithVueJs/webpack.config.js
@@ -0,0 +1,20 @@
+﻿module.exports = {
+    entry: {
+        contactEdit: "./scripts/src/contact/edit.js",
+        contactSearch: "./scripts/src/contact/search.js"
+    },
+    output: {
+        path: "./scripts/built",
+        filename: "[name].bundle.js"
+    },
+    externals: {
+        "Vue": "Vue"
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: "babel-loader",
+        }]
+    }
+}
